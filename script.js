7f584377d9c4410985e0759472a159cef5ed86ce
havenseyes <inesrcarvalho2005@gmail.com>
2025-02-13 19:15:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -76,45 +76,9 @@ function displayCatHeart() {
         imageContainer.appendChild(catHeartImage);
         // Hide the options container
         document.getElementById('options').style.display = 'none';
-
-        
     };
 document.getElementById("pt2").style.display="";
 }
 // Display the cat.gif initially
 
 displayCat();
-
-
-<button onclick="one()">1</button>
-<button onclick="two()">2</button>
-<button onclick="four()">4</button>
-
-<script>
-// Get the elements with class="column"
-var elements = document.getElementsByClassName("column1");
-
-// Declare a "loop" variable
-var i;
-
-// Full-width images
-function one() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "100%";
-  }
-}
-
-// Two images side by side
-function two() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "50%";
-  }
-}
-
-// Four images side by side
-function four() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "25%";
-  }
-}
-</script>
